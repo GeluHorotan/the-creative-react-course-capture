2616c3bbf198e392868b24635ac250e46f49c39d
Horotan Gelu <horotangelu17@gmail.com>
2022-04-07 21:58:02 +0300

    Animation

--- src/App.js
+++ src/App.js
@@ -10,18 +10,24 @@ import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
 
 // Router
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+// Animation
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <Routes>
-        <Route path='/' exact element={<AboutUs />} />
-        <Route path='/work' element={<OurWork />} />
-        <Route path='/contact' element={<ContactUs />} />
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' exact element={<AboutUs />} />
+          <Route path='/work' exact element={<OurWork />} />
+
+          <Route path='/contact' element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
